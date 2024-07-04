d6a1c9a7a28d8d61a2068b3e31b18b778e4c05a1
BearerOP <ankity1892003@gmail.com>
2024-07-05 00:00:47 +0530

    setup commit

---
+++ src/models/user_model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  auth_key: {
+    type: String,
+    default: null,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
