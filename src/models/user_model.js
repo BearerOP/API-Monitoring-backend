d0c915e658abd4d9ca5033a45239c70b03562d21
BearerOP <ankity1892003@gmail.com>
2024-07-07 17:52:38 +0530

    minor changes and modifications

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -9,10 +9,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  mobile: {
-    type: String,
-    required: true,
-  },
   password: {
     type: String,
     required: true,
