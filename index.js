cf1f12c8fa714839566da0076f36464a747edd23
BearerOP <ankity1892003@gmail.com>
2024-07-09 00:24:27 +0530

    bt

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Static files
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
