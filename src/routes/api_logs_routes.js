1d0a9fcf354d042f075e3780282c04e6de362b1e
BearerOP <ankity1892003@gmail.com>
2024-07-15 13:35:46 +0530

    added log deletion

--- src/routes/api_logs_routes.js
+++ src/routes/api_logs_routes.js
@@ -5,11 +5,14 @@ const user_auth = require("../../middleware/user_auth.js");
 
 const {
   addLogs,
+  deleteLogs,
   getLogs,
   getAllLogs,
 } = require("../controllers/api_logs_controller.js");
 
 router.post("/addLog", user_auth, addLogs);
+
+router.delete("/deleteLog", user_auth, deleteLogs);
 
 router.get("/getLogs", user_auth, getLogs);
 
